9ab6ee930e135656b6b2b42bb2654c6b0e626695
nestibry <nestibry@gmail.com>
2023-12-01 07:52:12 -0600

    init BrowswerRouter and the typical routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Header from './components/Header';
+
+// Temporary Pages for site navigation
+import { AboutPage, ContactPage, PortfolioPage, ResumePage, PageNotFound } from './pages/';
 
 export default function App() {
     return (
         <>
             <Header />
-            <h1>Vite + React</h1>
+            <BrowserRouter>
+                <Routes>
+                    <Route path='/' element={<AboutPage />} />
+                    <Route path='/portfolio' element={<PortfolioPage />} />
+                    <Route path='/resume' element={<ResumePage />} />
+                    <Route path='/contact' element={<ContactPage />} />
+                    <Route path='/*' element={<PageNotFound />} />
+                </Routes>
+            </BrowserRouter>
         </>
     );
 }
--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,4 +1,4 @@
-export default function AboutPage() {
+export default function ContactPage() {
     return (
         <>
             <h1>Contact</h1>
