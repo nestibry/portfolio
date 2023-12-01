392b198e1313bf69cef86f1d6f4024e179d53288
nestibry <nestibry@gmail.com>
2023-12-01 07:35:09 -0600

    init dummy pages for site navigation links

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,10 @@ import Header from './components/Header';
 
 export default function App() {
     return (
-        <div>
+        <>
             <Header />
             <h1>Vite + React</h1>
-        </div>
+        </>
     );
 }
 
