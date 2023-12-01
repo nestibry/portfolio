76fa500dc384acc8f7b599eb00f6914900b5e2d2
nestibry <nestibry@gmail.com>
2023-11-30 19:09:59 -0600

    init header.jsx and added the component to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import { useState } from 'react'
 // import './App.css'
 
+import Header from './components/Header';
+
 export default function App() {
-  return (
-    <>
-      
-      <h1>Vite + React</h1>
-      
-    </>
-  );
+    return (
+        <>
+            <Header />
+            <h1>Vite + React</h1>
+
+        </>
+    );
 }
 
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+export default function Header() {
+
+    return (
+        <>
+            <header>
+                <h1>Bryan Nestingen's Portfolio</h1>
+                <nav>
+                    <a href="#about-me">About Me</a>
+                    <a href="#work">Work</a>
+                    <a href="#contact-me">Contact Me</a>
+                </nav>
+            </header>
+        </>
+    );
+}
