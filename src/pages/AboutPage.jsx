21a7027a6c253c0dfe5e8d31c1df5de1e730e978
nestibry <nestibry@gmail.com>
2023-12-02 13:01:00 -0600

    converted the background-image content to a component called BackgroundImage

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,11 +1,11 @@
+import { BackgroundImage } from '../components/';
 import './AboutPage.css';
+
 
 export default function AboutPage() {
     return (
         <>
-            <section >
-                <div className="background-image" title="Shenandoah National Park, Photo Credits, Bryan Nestingen"></div>
-            </section>
+            <BackgroundImage />
 
             <section className="main-sections" id="about-me">
                 <h2 className='section-header'>About Me</h2>
