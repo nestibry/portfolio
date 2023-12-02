61778073b1c3d5e914babf4bb0d7864f4974f9d2
nestibry <nestibry@gmail.com>
2023-12-02 10:44:13 -0600

    init with about me content from original portfolio, profile image stored in the src/assets/image folderfor scalability

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,7 +1,28 @@
 export default function AboutPage() {
     return (
-        <>  
-            <h1>About Me</h1>
+        <>
+            <section className="main-sections" id="about-me">
+                <h2>About Me</h2>
+                <div className="section-content">
+                    <img src='../src/assets/images/bryan-nestingen-profile-photo.jpg' alt="Bryan Nestingen's profile photo." />
+                    <summary>
+                        <p>
+                            Naturally inquisitive, "Bryan with a Y" is the perfect moniker for me.
+                        </p>
+                        <p>
+                            Key Points:
+                        </p>
+                        <ul>
+                            <li>First and foremost, I'm a devoted father and husband.</li>
+                            <li>Being an avid outdoor enthusiast, I derive immense joy from hiking and mountain biking.</li>
+                            <li>I possess extensive expertise as a Software Engineer and Machine Vision Engineer, with a foundation in Electrical Engineering and a background in teaching Mathematics at the secondary level.</li>
+                        </ul>
+                        <p>
+                            Contact me to learn more about my eclectic background!
+                        </p>
+                    </summary>
+                </div>
+            </section>
         </>
     )
 }
