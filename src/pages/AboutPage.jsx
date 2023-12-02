57f0872073b488d608f75cef87cae5e1eb42b57b
nestibry <nestibry@gmail.com>
2023-12-02 14:18:18 -0600

    added p tag to the line items

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -19,9 +19,9 @@ export default function AboutPage() {
                             Key Points:
                         </p>
                         <ul>
-                            <li>First and foremost, I'm a devoted father and husband.</li>
-                            <li>Being an avid outdoor enthusiast, I derive immense joy from hiking and mountain biking.</li>
-                            <li>I possess extensive expertise as a Software Engineer and Machine Vision Engineer, with a foundation in Electrical Engineering and a background in teaching Mathematics at the secondary level.</li>
+                        <li><p>First and foremost, I'm a devoted father and husband.</p></li>
+                            <li><p>Being an avid outdoor enthusiast, I derive immense joy from hiking and mountain biking.</p></li>
+                            <li><p>I possess extensive expertise as a Software Engineer and Machine Vision Engineer, with a foundation in Electrical Engineering and a background in teaching Mathematics at the secondary level.</p></li>
                         </ul>
                         <p>
                             Contact me to learn more about my eclectic background!
