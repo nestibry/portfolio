5ec78379b8c8eecc38857941ae1698dcd422119c
nestibry <nestibry@gmail.com>
2023-12-05 13:31:10 -0600

    updated some styling for the profile and removed the p tags on the line items

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -5,16 +5,16 @@ export default function Profile() {
         <div className='profile-card'>
             <img className='profile-image' src='../src/assets/images/bryan-nestingen-profile-photo.jpg' alt="Bryan Nestingen's profile photo." />
             <summary className='profile-summary'>
-                <p>
+                <h6>
                     Naturally inquisitive, I go by 'Bryan with a Y' — a moniker that perfectly encapsulates my curiosity.
-                </p>
+                </h6>
                 <p>
                     Key Points:
                 </p>
                 <ul>
-                    <li><p>First and foremost, I'm a devoted father and husband.</p></li>
-                    <li><p>As an avid outdoor enthusiast, I find immense joy in activities like hiking and mountain biking.</p></li>
-                    <li><p>I am a versatile software engineer with a foundation in electrical engineering and a background in mathematics education. Seeking opportunities to leverage my expertise in software development, process automation, and full stack web development.</p></li>
+                    <li>First and foremost, I'm a devoted father and husband.</li>
+                    <li>As an avid outdoor enthusiast, I find immense joy in activities like hiking and mountain biking.</li>
+                    <li>I am a versatile software engineer with a foundation in electrical engineering and a background in mathematics education. Seeking opportunities to leverage my expertise in software development, process automation, and full stack web development.</li>
                 </ul>
                 <p>
                     Feel free to reach out to explore more about my diverse background! I look forward to connecting with you.
