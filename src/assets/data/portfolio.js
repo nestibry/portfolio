fde0ca9132b5c214fe797d024be75dc089c48b63
nestibry <nestibry@gmail.com>
2023-12-06 16:32:55 -0600

    portfolio is imported into the react component

---
+++ src/assets/data/portfolio.js
@@ -0,0 +1,29 @@
+export const portfolio = [
+    {
+        id: 1,
+        name: 'The Weather Dashboard',
+        stack: '',
+        img: '',
+        app: '',
+        repo: '',
+        demo: ''
+    },
+    {
+        id: 2,
+        name: 'The Tech Blog',
+        stack: '',
+        img: '',
+        app: '',
+        repo: '',
+        demo: ''
+    },
+    {
+        id: 3,
+        name: 'Code Quiz',
+        stack: '',
+        img: '',
+        app: '',
+        repo: '',
+        demo: ''
+    }
+];
