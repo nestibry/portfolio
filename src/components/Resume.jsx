729af294bf381d4afb75a3627bd0133647b405e2
nestibry <nestibry@gmail.com>
2023-12-06 20:33:26 -0600

    init download for the resume image

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -3,7 +3,9 @@ import './Profile.css';
 export default function Profile() {
     return (
         <div className='profile-card'>
-            <img className='profile-image' src='../src/assets/images/bryan-nestingen-profile-photo.jpg' alt="Bryan Nestingen's profile photo." />
+            <a href='../src/assets/images/bryan-nestingen-profile-photo.jpg' download>
+                <img className='profile-image' src='../src/assets/images/bryan-nestingen-profile-photo.jpg' alt="Bryan Nestingen's profile photo." />
+            </a>
             <summary className='profile-summary'>
                 <p>
                     Naturally inquisitive, I go by 'Bryan with a Y' — a moniker that perfectly encapsulates my curiosity.
