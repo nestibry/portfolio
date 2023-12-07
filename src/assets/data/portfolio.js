743b3300523a456990f142952775f5892f3a43e1
nestibry <nestibry@gmail.com>
2023-12-06 19:05:16 -0600

    reordered the projects

--- src/assets/data/portfolio.js
+++ src/assets/data/portfolio.js
@@ -1,26 +1,6 @@
 export const portfolio = [
     {
         id: 1,
-        name: 'Weather Dashboard',
-        description: 'Retrieves and displays 5-day weather forecast for cities using the OpenWeather API.',
-        stack: 'RESTful API, jQuery, Bootstrap',
-        img: 'weather-dashboard-example.png',
-        app: 'https://nestibry.github.io/umnbc-mod06-weather-dashboard/',
-        repo: 'https://github.com/nestibry/umnbc-mod06-weather-dashboard',
-        demo: ''
-    },
-    {
-        id: 2,
-        name: 'Code Quiz',
-        description: 'A timed multiple-choice coding assessment to simulate a coding assessment as part of an interview process.',
-        stack: 'HTML, CSS, JavaScript',
-        img: 'coding-quiz-example.png',
-        app: 'https://nestibry.github.io/umnbc-mod04-code-quiz/',
-        repo: 'https://github.com/nestibry/umnbc-mod04-code-quiz',
-        demo: ''
-    },
-    {
-        id: 3,
         name: 'The Tech Blog',
         description: 'A CMS-style blog site to publish articles, blog posts, and thoughts. The app follows the MVC paradigm in its architectural structure, using Handlebars.js as the templating language, Sequelize as the ORM, and the express-session npm package for authentication.',
         stack: 'MVC: Sequelize, Handlebars, Express.js, Express-Session',
@@ -30,7 +10,37 @@ export const portfolio = [
         demo: 'https://drive.google.com/file/d/17xze0yGHbymJomYxF7g4NTTApCZjhYZc/view'
     },
     {
+        id: 2,
+        name: 'Social Network API',
+        description: `An API for a social network web application where users can share their thoughts, react to friends' thoughts, and create a friend list.`,
+        stack: 'NoSQL: MongoDB, Mongoose, Express.js',
+        img: 'social-network-api-demo-video-thumbnail.png',
+        app: 'https://www.youtube.com/watch?v=mYROI-arFiY',
+        repo: 'https://github.com/nestibry/umnbc-mod18-nosql-social-network-api',
+        demo: 'https://www.youtube.com/watch?v=mYROI-arFiY'
+    },
+    {
+        id: 3,
+        name: 'Weather Dashboard',
+        description: 'Retrieves and displays 5-day weather forecast for cities using the OpenWeather API.',
+        stack: 'RESTful API, jQuery, Bootstrap',
+        img: 'weather-dashboard-example.png',
+        app: 'https://nestibry.github.io/umnbc-mod06-weather-dashboard/',
+        repo: 'https://github.com/nestibry/umnbc-mod06-weather-dashboard',
+        demo: ''
+    },
+    {
         id: 4,
+        name: 'Code Quiz',
+        description: 'A timed multiple-choice coding assessment to simulate a coding assessment as part of an interview process.',
+        stack: 'HTML, CSS, JavaScript',
+        img: 'coding-quiz-example.png',
+        app: 'https://nestibry.github.io/umnbc-mod04-code-quiz/',
+        repo: 'https://github.com/nestibry/umnbc-mod04-code-quiz',
+        demo: ''
+    },
+    {
+        id: 5,
         name: 'Employee Manager',
         description: `A command-line Content Management System (CMS) to manage a company's employee database, using Node.js, Inquirer, and MySQL.`,
         stack: 'MySQL, Node.js, Inquirer',
@@ -38,15 +48,5 @@ export const portfolio = [
         app: 'https://www.youtube.com/watch?v=R1OZm1962JI',
         repo: 'https://github.com/nestibry/umnbc-mod12-mysql-employee-manager',
         demo: 'https://www.youtube.com/watch?v=R1OZm1962JI'
-    },
-    {
-        id: 5,
-        name: 'The Social Network API',
-        description: `An API for a social network web application where users can share their thoughts, react to friends' thoughts, and create a friend list.`,
-        stack: 'NoSQL: MongoDB, Mongoose, Express.js',
-        img: 'social-network-api-demo-video-thumbnail.png',
-        app: 'https://www.youtube.com/watch?v=mYROI-arFiY',
-        repo: 'https://github.com/nestibry/umnbc-mod18-nosql-social-network-api',
-        demo: 'https://www.youtube.com/watch?v=mYROI-arFiY'
     }
 ];
