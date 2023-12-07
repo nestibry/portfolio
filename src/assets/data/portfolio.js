a7692dc58aa9bd6151bb65151136fc55a72675a2
nestibry <nestibry@gmail.com>
2023-12-06 19:00:07 -0600

    added data for social network api and employee manager

--- src/assets/data/portfolio.js
+++ src/assets/data/portfolio.js
@@ -31,12 +31,22 @@ export const portfolio = [
     },
     {
         id: 4,
-        name: 'The Tech Blog',
-        description: 'A CMS-style blog site to publish articles, blog posts, and thoughts. The app follows the MVC paradigm in its architectural structure, using Handlebars.js as the templating language, Sequelize as the ORM, and the express-session npm package for authentication.',
-        stack: 'MVC: Sequelize, Handlebars, Express.js, Express-Session',
-        img: 'the-tech-blog-example.png',
-        app: 'https://mysterious-basin-27960-f7cf6f2edc30.herokuapp.com/',
-        repo: 'https://github.com/nestibry/umnbc-mod14-mvc-tech-blog',
-        demo: 'https://drive.google.com/file/d/17xze0yGHbymJomYxF7g4NTTApCZjhYZc/view'
+        name: 'Employee Manager',
+        description: `A command-line Content Management System (CMS) to manage a company's employee database, using Node.js, Inquirer, and MySQL.`,
+        stack: 'MySQL, Node.js, Inquirer',
+        img: 'employee-manager-demo-video-thumbnail.png',
+        app: 'https://www.youtube.com/watch?v=R1OZm1962JI',
+        repo: 'https://github.com/nestibry/umnbc-mod12-mysql-employee-manager',
+        demo: 'https://www.youtube.com/watch?v=R1OZm1962JI'
+    },
+    {
+        id: 5,
+        name: 'The Social Network API',
+        description: `An API for a social network web application where users can share their thoughts, react to friends' thoughts, and create a friend list.`,
+        stack: 'NoSQL: MongoDB, Mongoose, Express.js',
+        img: 'social-network-api-demo-video-thumbnail.png',
+        app: 'https://www.youtube.com/watch?v=mYROI-arFiY',
+        repo: 'https://github.com/nestibry/umnbc-mod18-nosql-social-network-api',
+        demo: 'https://www.youtube.com/watch?v=mYROI-arFiY'
     }
 ];
