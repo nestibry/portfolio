b96d7c6ff240d0406b21cd9c2b0408a2132f877f
nestibry <nestibry@gmail.com>
2023-12-06 18:45:43 -0600

    added data for code quiz and tech blog

--- src/assets/data/portfolio.js
+++ src/assets/data/portfolio.js
@@ -7,6 +7,36 @@ export const portfolio = [
         img: 'weather-dashboard-example.png',
         app: 'https://nestibry.github.io/umnbc-mod06-weather-dashboard/',
         repo: 'https://github.com/nestibry/umnbc-mod06-weather-dashboard',
-        demo: 'https://github.com/nestibry/umnbc-mod06-weather-dashboard'
+        demo: ''
+    },
+    {
+        id: 2,
+        name: 'Code Quiz',
+        description: 'A timed multiple-choice coding assessment to simulate a coding assessment as part of an interview process.',
+        stack: 'HTML, CSS, JavaScript',
+        img: 'coding-quiz-example.png',
+        app: 'https://nestibry.github.io/umnbc-mod04-code-quiz/',
+        repo: 'https://github.com/nestibry/umnbc-mod04-code-quiz',
+        demo: ''
+    },
+    {
+        id: 3,
+        name: 'The Tech Blog',
+        description: 'A CMS-style blog site to publish articles, blog posts, and thoughts. The app follows the MVC paradigm in its architectural structure, using Handlebars.js as the templating language, Sequelize as the ORM, and the express-session npm package for authentication.',
+        stack: 'MVC: Sequelize, Handlebars, Express.js, Express-Session',
+        img: 'the-tech-blog-example.png',
+        app: 'https://mysterious-basin-27960-f7cf6f2edc30.herokuapp.com/',
+        repo: 'https://github.com/nestibry/umnbc-mod14-mvc-tech-blog',
+        demo: 'https://drive.google.com/file/d/17xze0yGHbymJomYxF7g4NTTApCZjhYZc/view'
+    },
+    {
+        id: 4,
+        name: 'The Tech Blog',
+        description: 'A CMS-style blog site to publish articles, blog posts, and thoughts. The app follows the MVC paradigm in its architectural structure, using Handlebars.js as the templating language, Sequelize as the ORM, and the express-session npm package for authentication.',
+        stack: 'MVC: Sequelize, Handlebars, Express.js, Express-Session',
+        img: 'the-tech-blog-example.png',
+        app: 'https://mysterious-basin-27960-f7cf6f2edc30.herokuapp.com/',
+        repo: 'https://github.com/nestibry/umnbc-mod14-mvc-tech-blog',
+        demo: 'https://drive.google.com/file/d/17xze0yGHbymJomYxF7g4NTTApCZjhYZc/view'
     }
 ];
