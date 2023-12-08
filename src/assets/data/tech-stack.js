adf64e4a481dccf8342eebe16aa48356a6fc6f6a
nestibry <nestibry@gmail.com>
2023-12-07 19:26:02 -0600

    init techStack

---
+++ src/assets/data/tech-stack.js
@@ -0,0 +1,13 @@
+export const techStack = [
+    'HTML, CSS, JavaScript, Python, C#',
+    'Bootstrap and other CSS frameworks',
+    'React',
+    'NodeJS',
+    'Express',
+    'MongoDB, Mongoose',
+    'MSSQL, MySQL, Sequelize',
+    'Jest',
+    'Cognex InSight',
+    'Savigent Workflow',
+    'Power Apps, Power Automate, Power BI'
+];
