82ff4786af6d19a643af83fe3a2dbc7a53e8078b
nestibry <nestibry@gmail.com>
2023-12-09 00:59:32 -0600

    classnames for form fields

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -10,28 +10,33 @@ export default function Resume() {
         setUserData({ ...userData, [e.target.name]: e.target.value });
     }
 
-    async function handleFormSubmit(e) {
-        e.prevendDefault();
+    function handleReset() {
+        setUserData({ ...userData, name: "", email: "", message: "" });
     }
 
+    async function handleFormSubmit(e) {
+        e.preventDefault();
+        console.log(userData);
+    }
 
     return (
 
         <div className='contact-card'>
             <h3>Contact Form</h3>
             <form className='contact-form' onSubmit={handleFormSubmit}>
-                <label>Name</label>
-                <input type="text" name="name" value={userData.name} onChange={handleInputChange} />
-
-                <label>Email Address</label>
-                <input type="text" name="email" value={userData.email} onChange={handleInputChange} />
-
-                <label>Password</label>
-                <input type="text" name="message" value={userData.message} onChange={handleInputChange} />
-
-                <label className='form-error-message'>Error</label>
-
-                <button className="form-submit">Submit</button>
+                <div className='form-fields'>
+                    <label>Name</label>
+                    <input type="text" name="name" value={userData.name} onChange={handleInputChange} />
+                    <label>Email Address</label>
+                    <input type="text" name="email" value={userData.email} onChange={handleInputChange} />
+                    <label>Message</label>
+                    <textarea type="text" name="message" value={userData.message} onChange={handleInputChange} />
+                    <label>Error</label>
+                </div>
+                <div className='form-buttons'>
+                    <button type="reset" onClick={handleReset}>Clear All</button>
+                    <button>Submit</button>
+                </div>
             </form>
         </div>
 
