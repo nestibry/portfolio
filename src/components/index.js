3f7081531b95efe05cff505956c7b7310e982b5f
nestibry <nestibry@gmail.com>
2023-12-08 23:33:33 -0600

    init contact page

--- src/components/index.js
+++ src/components/index.js
@@ -4,5 +4,6 @@ export { default as BackgroundImage} from './BackgroundImage';
 export { default as Profile } from './Profile';
 export { default as Portfolio } from './Portfolio';
 export { default as Resume } from './Resume';
+export { default as Contact } from './Contact';
 
 export { default as Footer} from './Footer';
